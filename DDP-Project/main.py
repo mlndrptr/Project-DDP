acfa35456da0dccd0e3bcff6cae1cf270b32c5f0
mlndrptr <0110124132@student.nurulfikri.ac.id>
2024-12-20 22:01:56 +0700

    Add files via upload

---
+++ DDP-Project/main.py
@@ -0,0 +1,60 @@
+import streamlit as st
+import requests
+
+# Fungsi untuk mendapatkan data cuaca
+def get_weather_data(location):
+    api_key = "cfd9a0d39ee02d75246d54a0442eb015"
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}&units=metric&lang=id"
+    response = requests.get(url)
+    return response.json()
+
+# Halaman utama
+st.title("Aplikasi Cuaca Jabodetabek")
+st.write("Selamat datang di aplikasi cuaca Jabodetabek! Silakan pilih lokasi dan cek informasi cuaca.")
+
+# Sidebar untuk memilih lokasi
+st.sidebar.header("Cek Lokasi Wilayah")
+location = st.sidebar.selectbox("Pilih wilayah:", ["Jakarta", "Bogor", "Depok", "Tangerang", "Bekasi"])
+
+# Variabel untuk menyimpan data cuaca
+weather_data = None
+
+# Sidebar untuk cek suhu
+st.sidebar.header("Cek Suhu")
+if st.sidebar.button("Cek Suhu"):
+    weather_data = get_weather_data(location)
+    if weather_data.get("main"):
+        temperature = weather_data["main"]["temp"]
+        st.write(f"Suhu di {location} adalah {temperature}°C")
+    else:
+        st.write("Data cuaca tidak ditemukan.")
+
+# Sidebar untuk cek cuaca
+#st.sidebar.header("Cek Cuaca")
+if st.sidebar.button("Cek Cuaca"):
+    if weather_data is None:
+        weather_data = get_weather_data(location)
+    if weather_data.get("weather"):
+        weather_condition = weather_data["weather"][0]["description"]
+        st.write(f"Kondisi cuaca di {location} adalah {weather_condition}")
+    else:
+        st.write("Data cuaca tidak ditemukan.")
+
+# Sidebar untuk rekomendasi
+st.sidebar.header("Rekomendasi")
+
+if weather_data is not None and weather_data.get("weather"):
+    if "hujan" in weather_data["weather"][0]["description"]:
+        st.write("Rekomendasi: Bawa payung atau jas hujan saat keluar")
+    elif "langit cerah" in weather_data["weather"][0]["description"]:
+        st.write("Rekomendasi: Cuaca Cerah!")
+    elif "cerah" in weather_data["weather"][0]["description"]:
+        st.write("Rekomendasi: Cuaca cerah! Gunakan pakaian ringan dan jangan lupa sunscreen.")
+    elif "mendung" in weather_data["weather"][0]["description"]:
+        st.write ("Rekomendasi: Cuaca mendung, Anda mungkin memerlukan jaket ringan.")
+    elif "badai petir"in weather_data["weather"][0]["description"]:
+        st.write("Rekomendasi: Hati-hati dengan petir dan hujan lebat, sebaiknya tetap di dalam ruangan.")
+    else:
+        st.write ("Cuaca normal. Nikmati hari Anda!")
+else:
+    st.write("Silakan cek suhu atau cuaca terlebih dahulu.")
